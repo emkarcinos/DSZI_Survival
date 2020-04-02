5e33e553968ea2064e33cde7ba8b59b05cd257aa
Marcin Kostrzewski <komar86311@gmail.com>
2020-04-02 15:50:47 +0200

    initialized pygame

--- src/game/Game.py
+++ src/game/Game.py
@@ -1,6 +1,28 @@
+import pygame
+import json
+from pathlib import Path
+
+
 class Game:
     def __init__(self):
-        self.resolution
-        self.windowName
-        self.fps
-        self.timer
+        print("Loading configuration...", end=" ")
+
+        try:
+            configFolder = Path("../../data/config/")
+            configFile = configFolder / "mainConfig.json"
+
+            self.config = json.loads(configFile.read_text())
+
+            print("OK")
+        except IOError:
+            print("Error reading configuration file. Exiting...")
+            exit(1)
+
+        print("Initializing pygame...", end=" ")
+        pygame.init()
+        print("OK")
+
+        print("Initializing screen, params: " + str(self.config["window"]) + "...", end=" ")
+
+
+game = Game()
