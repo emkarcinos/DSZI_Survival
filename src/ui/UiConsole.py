e8a961cdddbe238dde5c67e463114578f35b197b
Michał Czekański <michal.cz2202@gmail.com>
2020-04-04 18:40:56 +0200

    Fix generating UiConsole image

--- src/ui/UiConsole.py
+++ src/ui/UiConsole.py
@@ -4,9 +4,9 @@ from src.ui.UiElement import UiElement
 
 
 class UiConsole(UiElement):
-    def __init__(self, rect, bgColor=(125, 125, 125)):
+    def __init__(self, rect: pygame.Rect, bgColor=(125, 125, 125)):
         super().__init__(rect)
         self.bgColor = bgColor
 
-        self.image = pygame.Surface(rect)
+        self.image = pygame.Surface((rect.width, rect.height))
         self.image.fill(bgColor)
