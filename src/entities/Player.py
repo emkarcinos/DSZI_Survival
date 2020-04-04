d16711068a2dc80f4d68e8e650c1cbc0cf71dad9
Marcin Kostrzewski <komar86311@gmail.com>
2020-04-04 20:54:11 +0200

    Initial player implementation

--- src/entities/Player.py
+++ src/entities/Player.py
@@ -1,6 +1,24 @@
-import src.entities.Entity as Entity
+from enum import Enum
+
+from src.entities.Entity import Entity
+from src.entities.Statistics import Statistics
+import pygame
 
 
-class Player(Entity):
-    def __init__(self):
-        self.statistics
+class Player(Entity, pygame.sprite.Sprite):
+    def __init__(self, spawnpoint, size):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.statistics = Statistics(100, 0, 0, 100)
+
+        self.image, self.rect = Entity.getTexture("player.jpg", size)
+        super(Player, self).__init__(self.image, spawnpoint)
+        # Where the player is facing, 0 - north, 1
+        self.rotation = Rotations.NORTH
+
+
+class Rotations(Enum):
+    NORTH = 0
+    EAST = 1
+    SOUTH = 2
+    WEST = 3
