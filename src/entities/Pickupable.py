057f711317736ebc28fc1dd6b0962d470c575c3d
jonspacz <jonathanspaczynski@gmail.com>
2020-04-05 20:34:45 +0200

    Removed Collidable because was not useful finished todo in pickupable and added Statistics to contstructor

--- src/entities/Pickupable.py
+++ src/entities/Pickupable.py
@@ -1,16 +1,17 @@
 import src.entities.Interactable as Interactable
 from src.entities import Statistics, Player
-
+from game.MapNew import Map
 
 class Pickupable(Interactable):
-    def __init__(self, texture, pos, id):
+    def __init__(self, texture, pos, id, Statistics):
         super().__init__(texture, pos, id)
         self.is_pickupable = True
+        self.Statistics = Statistics
 
-    def on_pickup(self, Player, Statistics):
-        Player.statistics.set_hp(Statistics.hp)
-        Player.statistics.set_stamina(Statistics.stamina)
-        Player.statistics.set_thirst(Statistics.thirst)
-        Player.statistics.set_hunger(Statistics.hunger)
+    def on_pickup(self, Player):
+        Player.statistics.set_hp(self.Statistics.hp)
+        Player.statistics.set_stamina(self.Statistics.stamina)
+        Player.statistics.set_thirst(self.Statistics.thirst)
+        Player.statistics.set_hunger(self.Statistics.hunger)
 
-        # TODO delete pickupable object from map
+        Map.removeSpriteFromMap(self)
