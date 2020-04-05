404d2f1b50eae3f342ec74328bf994d71f59a790
jonspacz <jonathanspaczynski@gmail.com>
2020-04-05 21:36:58 +0200

    deleted not needed Map elements from Pickupable

--- src/entities/Pickupable.py
+++ src/entities/Pickupable.py
@@ -1,6 +1,5 @@
 import src.entities.Interactable as Interactable
 from src.entities import Statistics, Player
-from game.MapNew import Map
 
 class Pickupable(Interactable):
     def __init__(self, texture, pos, id, Statistics):
@@ -14,4 +13,3 @@ class Pickupable(Interactable):
         Player.statistics.set_thirst(self.Statistics.thirst)
         Player.statistics.set_hunger(self.Statistics.hunger)
 
-        Map.removeSpriteFromMap(self)
