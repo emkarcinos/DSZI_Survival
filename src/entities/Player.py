a264ac7cb19edfeda4b57a98e603ed0a484706be
Marcin Kostrzewski <komar86311@gmail.com>
2020-04-05 14:52:30 +0200

    Refactor to fixed Entity class

--- src/entities/Player.py
+++ src/entities/Player.py
@@ -7,10 +7,7 @@ import pygame
 
 class Player(Entity):
     def __init__(self, spawnpoint, size):
-        self.statistics = Statistics(100, 0, 0, 100)
-
-        self.image, self.rect = Entity.getTexture("player.jpg", size)
-        super().__init__(self.image, spawnpoint)
+        super().__init__("player.jpg", size, spawnpoint)
         # Where the player is facing, 0 - north, 1
         self.rotation = Rotations.NORTH
 
