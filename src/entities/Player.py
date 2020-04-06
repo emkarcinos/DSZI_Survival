833979740917856b6f74fde3e69a02e208675115
Marcin Kostrzewski <komar86311@gmail.com>
2020-04-06 09:56:07 +0200

    Removed unnecessary line

--- src/entities/Player.py
+++ src/entities/Player.py
@@ -6,8 +6,6 @@ import pygame
 
 
 class Player(Entity):
-    statistics: Statistics
-
     def __init__(self, spawnpoint, size):
         super().__init__("player.png", size, (spawnpoint[0] * size, spawnpoint[1] * size))
         # Where the player is facing, 0 - north, 1
