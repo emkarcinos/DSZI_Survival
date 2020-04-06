c6ab7cc2f46fc66f1387e19ce84b7c39aa7c3fc3
Marcin Kostrzewski <komar86311@gmail.com>
2020-04-06 10:29:43 +0200

    Refactor: interactable now has methods from pickupable

--- src/entities/Pickupable.py
+++ src/entities/Pickupable.py
@@ -1,4 +1,3 @@
-from src.entities import Statistics, Player
 from src.entities.Interactable import Interactable
 
 
@@ -6,11 +5,4 @@ class Pickupable(Interactable):
     def __init__(self, texture, size, pos, Statistics):
         super().__init__(texture, size, pos)
         self.is_pickupable = True
-        self.Statistics = Statistics
-
-    def on_pickup(self, Player):
-        Player.statistics.set_hp(self.Statistics.hp)
-        Player.statistics.set_stamina(self.Statistics.stamina)
-        Player.statistics.set_thirst(self.Statistics.thirst)
-        Player.statistics.set_hunger(self.Statistics.hunger)
 
