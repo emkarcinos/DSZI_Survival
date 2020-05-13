28d9013983c834b39d668346227e41151ee2a4ba
Marcin Kostrzewski <komar86311@gmail.com>
2020-05-13 19:36:06 +0200

    Enums refactor

---
+++ src/entities/Enums.py
@@ -0,0 +1,22 @@
+from enum import Enum
+
+
+class Rotations(Enum):
+    NORTH = 0
+    EAST = 1
+    SOUTH = 2
+    WEST = 3
+
+
+class StatisticNames(Enum):
+    HP = 0
+    STAMINA = 1
+    HUNGER = 2
+    THIRST = 3
+
+
+class Movement(Enum):
+    ROTATE_R = 0
+    ROTATE_L = 1
+    FORWARD = 2
+    PICKUP = 3
