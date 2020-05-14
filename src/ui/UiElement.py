f0d6f5f8e44940d869405b94e627b6c6e482998e
Michał Czekański <michal.cz2202@gmail.com>
2020-05-14 21:10:09 +0200

    Add comments in UiElement.py

--- src/ui/UiElement.py
+++ src/ui/UiElement.py
@@ -2,6 +2,12 @@ import pygame
 
 
 class UiElement(pygame.sprite.Sprite):
-    def __init__(self, rect):
+    rect: pygame.Rect
+
+    def __init__(self, rect: pygame.Rect):
+        """
+        Creates UiElement object.
+        :param rect: UiElement will be drawn on this rectangle.
+        """
         super().__init__()
         self.rect = rect
