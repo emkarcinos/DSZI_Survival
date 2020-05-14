aab779d4e1a16d9f5536c964f73b966c26955674
Michał Czekański <michal.cz2202@gmail.com>
2020-05-14 21:18:57 +0200

    Add comments in UiImage.py

--- src/ui/UiImage.py
+++ src/ui/UiImage.py
@@ -3,7 +3,14 @@ import pygame
 
 
 class UiImage(UiElement):
+    image: pygame.Surface
+
     def __init__(self, rect: pygame.Rect, image: pygame.Surface):
+        """
+        Creates UiImage object.
+        :param rect: Rectangle on which image will be displayed
+        :param image: Image to display
+        """
         super().__init__(rect)
         self.image = pygame.transform.scale(image, (rect.width, rect.height))
         
