fd54303dba73e086c61367d5e8bd0c9be0216088
Michał Czekański <michal.cz2202@gmail.com>
2020-05-21 16:57:33 +0200

    Change type of branch field in DecisionTree class
    Type was Any, now it is DecisionTreeBranch.

--- src/AI/DecisionTrees/DecisionTree.py
+++ src/AI/DecisionTrees/DecisionTree.py
@@ -1,11 +1,12 @@
 from typing import List, Any
 
 from src.AI.DecisionTrees.AttributeDefinition import AttributeDefinition
+from src.AI.DecisionTrees.DecisionTreeBranch import DecisionTreeBranch
 
 
 class DecisionTree(object):
     root: AttributeDefinition
-    branches: List[Any]
+    branches: List[DecisionTreeBranch]
 
     def __init__(self, root):
         self.root = root
