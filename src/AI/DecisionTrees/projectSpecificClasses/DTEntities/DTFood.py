94ec3c273eba87ef057f924a7551938e5dcac0ce
Michał Czekański <michal.cz2202@gmail.com>
2020-05-23 22:10:05 +0200

    Remove unnecessary DTEntities and add classification in general class

--- src/AI/DecisionTrees/projectSpecificClasses/DTEntities/DTFood.py
+++
@@ -1,9 +0,0 @@
-from src.AI.DecisionTrees.projectSpecificClasses.DTEntities.DTSurvivalEntity import DTSurvivalEntity
-from src.AI.DecisionTrees.projectSpecificClasses.DistFromObject import DistFromObject
-from src.AI.DecisionTrees.projectSpecificClasses.SurvivalClassification import SurvivalClassification
-
-
-class DTFood(DTSurvivalEntity):
-    def __init__(self, distanceFromPlayer: DistFromObject):
-        super().__init__(distanceFromPlayer)
-        self.classification = SurvivalClassification.FOOD
--- src/AI/DecisionTrees/projectSpecificClasses/DTEntities/DTSurvivalEntity.py
+++ src/AI/DecisionTrees/projectSpecificClasses/DTEntities/DTSurvivalEntity.py
@@ -1,8 +1,10 @@
 from src.AI.DecisionTrees.projectSpecificClasses.DistFromObject import DistFromObject
+from src.AI.DecisionTrees.projectSpecificClasses.SurvivalClassification import SurvivalClassification
 
 
 class DTSurvivalEntity:
     DistFromObjectFromPlayer: DistFromObject
 
-    def __init__(self, distanceFromPlayer: DistFromObject):
+    def __init__(self, distanceFromPlayer: DistFromObject, classification: SurvivalClassification):
+        self.classification = classification
         self.distanceFromPlayer = distanceFromPlayer
