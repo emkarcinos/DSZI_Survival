8d4cde8eb0bc8f6d2e93a46232ad37ea2fcf7d50
Michał Czekański <michal.cz2202@gmail.com>
2020-05-23 17:15:44 +0200

    Add enum for classifying dt survival examples.
    This enum will be used in decision tree learning. For example food classification will mean that player should go for food.

---
+++ src/AI/DecisionTrees/projectSpecificClasses/SurvivalClassification.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class SurvivalClassification(Enum):
+    """
+    For example food classification means that player should go for food.
+    """
+
+    FOOD = 0,
+    WATER = 1,
+    REST = 2
