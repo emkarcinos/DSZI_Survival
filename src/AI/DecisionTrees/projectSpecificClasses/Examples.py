7b6a46188b8ac125acab661b34c6b41bb3a158da
Michał Czekański <michal.cz2202@gmail.com>
2020-05-24 15:48:32 +0200

    Remove wrong DT examples, leave them empty for now.

--- src/AI/DecisionTrees/projectSpecificClasses/Examples.py
+++ src/AI/DecisionTrees/projectSpecificClasses/Examples.py
@@ -4,20 +4,4 @@ from src.AI.DecisionTrees.projectSpecificClasses.PlayerStatsValue import PlayerS
 from src.AI.DecisionTrees.projectSpecificClasses.DistFromObject import DistFromObject as Dist
 
 examples = [
-    SurvivalDTExample(SurvivalClassification.WATER,
-                      Stats.HALF_TO_THREE_QUARTERS,
-                      Stats.QUARTER_TO_HALF,
-                      Stats.QUARTER_TO_HALF,
-                      Dist.GE_3_LT_8,
-                      Dist.GE_8,
-                      Dist.LT_3),
-
-    SurvivalDTExample(SurvivalClassification.FOOD,
-                      Stats.HALF_TO_THREE_QUARTERS,
-                      Stats.QUARTER_TO_HALF,
-                      Stats.QUARTER_TO_HALF,
-                      Dist.LT_3,
-                      Dist.GE_8,
-                      Dist.GE_8),
-
 ]
